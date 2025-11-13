7283822f1afaea8abeab4b785ea5e30c06d278e1
Wasim <imwasim.shaik@gmail.com>
2025-11-13 06:43:10 +0530

    aws_module_creation

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "ami_id"{
+    type = string
+    description = "this is the AMI used for creating EC2 instance"
+}
+
+variable "instance_type"{
+    type = string
+    description = "Instance type used for creating EC2 instance"
+    validation {
+        condition = contains(["t3.micro","t3.small","t3.medium"], var.instance_type)
+        error_message = "Please select either t3 micro or small or medium"
+    }
+}
+
+variable "sg_ids" {
+    type = list 
+    description = "this are the security group ids used for creating EC2 instance"  
+}
+
+variable "tags" {
+    type = map 
+    default = {}  
+}
