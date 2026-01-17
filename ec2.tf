9e819f2760f860d0ee22c44b524cb9a629d2e48c
Wasim <imwasim.shaik@gmail.com>
2026-01-17 06:14:52 +0530

    terraform message

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "this"{
     ami = var.ami_id
     instance_type = var.instance_type
-    security_groups = var.sg_ids
+    vpc_security_group_ids = var.sg_ids
     tags = var.tags
 }
